00ca4f109524d297cdb459906e4bae5bc72e1e24
flenn <deswier@bk.ru>
2020-06-04 21:46:44 +0700

    Refactoring

--- src/app/GameProcess/NewGame.java
+++ src/app/GameProcess/NewGame.java
@@ -1,16 +1,32 @@
 package app.GameProcess;
 
-import app.GameProcess.Round;
-import app.GameProcess.SpawnWord;
-import app.GameProcess.RandomLineNumber;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.IOException;
 
-public class NewGame {
+public class NewGame implements KeyListener {
+    private int difficutly = SpawnWord.dataBase();
+    private String word = RandomLineNumber.getWordFromFile(difficutly);
+    private Round round = new Round(word);
 
-    int difficutly=SpawnWord.dataBase();
-    String word=RandomLineNumber.getWordFromFile(difficutly);
-    Round round=new Round(word);
     public NewGame() throws IOException {
-        round.process();
+        // round.process();
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent keyEvent) {
+        System.out.println("escape");
+        if (keyEvent.getKeyCode() == KeyEvent.VK_CAPS_LOCK)
+            round.process();
+    }
+
+    @Override
+    public void keyReleased(KeyEvent keyEvent) {
+
     }
 }
--- src/app/GameProcess/RandomLineNumber.java
+++ src/app/GameProcess/RandomLineNumber.java
@@ -2,32 +2,29 @@ package app.GameProcess;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.LineNumberReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class RandomLineNumber {
+class RandomLineNumber {
     private static int linesCount = 0;
 
-    public static String getWordFromFile(int difficulty) throws IOException {
-        String getWord = null;
-        List<String> listOfWords=new ArrayList<String>();
-        try (FileReader getWordFromFile = new FileReader(".\\src\\app\\BaseWords\\" + difficulty + ".txt");
+    static String getWordFromFile(int difficulty) {
+        String getWord;
+        List<String> listOfWords = new ArrayList<>();
+        try (FileReader file = new FileReader(".\\src\\app\\BaseWords\\" + difficulty + ".txt")
         ) {
-            Scanner getLineWord = new Scanner(getWordFromFile);
+            Scanner getLineWord = new Scanner(file);
             while (getLineWord.hasNextLine()) {
-                String lineWord=getLineWord.nextLine();
+                String lineWord = getLineWord.nextLine();
                 listOfWords.add(lineWord);
                 linesCount++;
             }
-            int lineWord = (int) (Math.random() * linesCount) + 0;
-            getWord=listOfWords.get(lineWord);
+            int lineWord = (int) (Math.random() * linesCount);
+            getWord = listOfWords.get(lineWord);
         } catch (IOException ex) {
-
-            System.out.println(ex.getMessage());
+            throw new IllegalArgumentException("File can't be open!");
         }
-
         return getWord;
     }
 }
